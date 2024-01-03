7575a24b84c31bbc6bc510424c4e69749ab6cfc2
Alexandr Holub <a.holub@moonmana.com>
2024-01-03 17:04:52 +0200

    add all animals

---
+++ Assets/Scripts/Patterns/Strategy/Animals/Ant.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Patterns.Strategy.Behaviours;
+using Patterns.Strategy.MiniGames.GuessWho;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Patterns.Strategy.Animals
+{
+    public class Ant : AnimalBase
+    {
+        [SerializeField] private string _food;
+        [SerializeField] private string _walk;
+        [SerializeField] private string _sleep;
+
+        private Button _button;
+        //---------------------------------------------------------------------------------------------------------------
+        private void Awake()
+        {
+            SetEatBehaviour(new EatBehaviour(_food));
+            SetWalkBehaviour(new WalkBehaviour(_walk));
+            SetSleepBehaviour(new SleepBehaviour(_sleep));
+
+            Behaviour = new Dictionary<string, Action>()
+            {
+                {"eat", Eat},
+                {"walk", Walk},
+                {"sleep", Sleep}
+            };
+
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(MarkSelected);
+        }
+        //---------------------------------------------------------------------------------------------------------------
+        private void MarkSelected()
+        {
+            GameSignalService.SelectAnimal(this);
+            GameSignalService.SetButton(_button);
+        }
+        //---------------------------------------------------------------------------------------------------------------
+    }
+}
---
+++ Assets/Scripts/Patterns/Strategy/Animals/Perch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Patterns.Strategy.Behaviours;
+using Patterns.Strategy.MiniGames.GuessWho;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Patterns.Strategy.Animals
+{
+    public class Perch : AnimalBase
+    {
+        [SerializeField] private string _food;
+        [SerializeField] private string _sleep;
+
+        private Button _button;
+        //---------------------------------------------------------------------------------------------------------------
+        private void Awake()
+        {
+            SetEatBehaviour(new EatBehaviour(_food));
+            SetSleepBehaviour(new SleepBehaviour(_sleep));
+
+            Behaviour = new Dictionary<string, Action>()
+            {
+                {"eat", Eat},
+                {"sleep", Sleep}
+            };
+
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(MarkSelected);
+        }
+        //---------------------------------------------------------------------------------------------------------------
+        private void MarkSelected()
+        {
+            GameSignalService.SelectAnimal(this);
+            GameSignalService.SetButton(_button);
+        }
+        //---------------------------------------------------------------------------------------------------------------
+    }
+}
