65e4c9634ac2f5a752272471645e443f476b7426
Alexandr Holub <a.holub@moonmana.com>
2024-01-11 21:02:33 +0200

    Move me : start add graphic

---
+++ Assets/Scripts/Patterns/Strategy/Move Me/Units/Slime.cs
@@ -0,0 +1,36 @@
+﻿using Patterns.Strategy.Move_Me.Behaviours;
+using UnityEngine;
+
+namespace Patterns.Strategy.Move_Me.Units
+{
+    public class Slime : UnitBase
+    {
+        [SerializeField] private Transform[] _path;
+        //---------------------------------------------------------------------------------------------------------------
+        private void Awake()
+        {
+            Vector3[] path = new Vector3[_path.Length];
+
+            for (int i = 0; i < path.Length; i++)
+            {
+                path[i] = _path[i].position;
+            }
+            
+            SetMoveBehaviour(new Patrol(transform, path, _speed));
+        }
+        //---------------------------------------------------------------------------------------------------------------
+        private void Start()
+        {
+            transform.position = _path[0].position;
+        }
+        //---------------------------------------------------------------------------------------------------------------
+        private void Update()
+        {
+            if (_hasCommandMove)
+            { 
+                Move();
+            }
+        }
+        //---------------------------------------------------------------------------------------------------------------
+    }
+}
