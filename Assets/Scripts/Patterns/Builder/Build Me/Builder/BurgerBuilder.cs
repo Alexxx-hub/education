27b25e5cd803463911fbfe489b83fb1b947f3a8e
Alexxx-hub <alex.dark.lightning@gmail.com>
2024-01-31 14:58:45 +0200

    Start Builder pattern
    finish My little factory
    start Build Me

---
+++ Assets/Scripts/Patterns/Builder/Build Me/Builder/BurgerBuilder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class BurgerBuilder
+{
+    public Func<BurgerBuilder>[] actions;
+    
+    public bool item1;
+    public bool item2;
+    public bool item3;
+    public bool item4;
+    //---------------------------------------------------------------------------------------------------------------
+    public BurgerBuilder()
+    {
+        actions = new Func<BurgerBuilder>[]
+        {
+            WithItem1,
+            WithItem2,
+            WithItem3,
+            WithItem4
+        };
+    }
+    //---------------------------------------------------------------------------------------------------------------
+    private BurgerBuilder Default()
+    {
+        item1 = false;
+        item2 = false;
+        item3 = false;
+        item4 = false;
+        return this;
+    }
+    //---------------------------------------------------------------------------------------------------------------
+    private BurgerBuilder WithItem1()
+    {
+        item1 = true;
+        return this;
+    }
+    //---------------------------------------------------------------------------------------------------------------
+    private BurgerBuilder WithItem2()
+    {
+        item2 = true;
+        return this;
+    }
+    //---------------------------------------------------------------------------------------------------------------
+    private BurgerBuilder WithItem3()
+    {
+        item3 = true;
+        return this;
+    }
+    //---------------------------------------------------------------------------------------------------------------
+    private BurgerBuilder WithItem4()
+    {
+        item4 = true;
+        return this;
+    }
+    //---------------------------------------------------------------------------------------------------------------
+    public BurgerBuilder SetItems(int[] settings)
+    {
+        Default();
+        
+        foreach (var parametr in settings)
+        {
+            actions[parametr].Invoke();
+        }
+
+        return this;
+    }
+    //---------------------------------------------------------------------------------------------------------------
+    public Burger Build()
+    {
+        Burger burger = Object.Instantiate(new GameObject()).AddComponent<Burger>();
+        burger.SetItem1(item1);
+        burger.SetItem2(item2);
+        burger.SetItem3(item3);
+        burger.SetItem4(item4);
+        return burger;
+    }
+    //---------------------------------------------------------------------------------------------------------------
+}
---
+++ Assets/Scripts/Patterns/Builder/Build Me/Core/LevelService.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class LevelService : MonoBehaviour
+{
+    public int[] settings;
+    
+    private Burger _burger;
+    private BurgerBuilder _burgerBuilder;
+    //---------------------------------------------------------------------------------------------------------------
+    private void Awake()
+    {
+        _burgerBuilder = new BurgerBuilder();
+    }
+    //---------------------------------------------------------------------------------------------------------------
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            _burger = _burgerBuilder
+                .SetItems(settings)
+                .Build();
+            _burger.GetBurger();
+        }
+    }
+    //---------------------------------------------------------------------------------------------------------------
+}
